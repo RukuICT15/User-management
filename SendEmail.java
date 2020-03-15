b08ba5b26e7fcf1a087794d150da41bf263a5f60
Ruku Shikder <57752923+RukuICT15@users.noreply.github.com>
2020-03-14 18:39:20 -0700

    added additional jar file
    init commit

---
+++ SendEmail.java
@@ -0,0 +1,98 @@
+package com.hateyahighschool.service;
+
+/**
+ * Created by A.A.MAMUN on 8/9/2019.
+ */
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendEmail {
+
+    public static void sendEmail(String toEmail,String subject,String  message)
+    {
+
+        try
+        {
+
+            String fromEmail="";
+            String username="";
+            String password="";
+
+            Properties props= System.getProperties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.fallback", "false");
+            //props.put("mail.smtp.starttls.enable", "true");
+            props.put( "mail.smtp.ssl.enable", "true");
+
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(true);
+
+            Message mailMessage=new MimeMessage(mailSession);
+            mailMessage.setFrom(new InternetAddress(fromEmail));
+            mailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            //mailMessage.setContent(message, "html/text");
+            mailMessage.setContent(message, "text/plain");
+            mailMessage.setSubject(subject);
+
+            Transport transport=mailSession.getTransport("smtp");
+            transport.connect("smtp.gmail.com",username,password);
+            transport.sendMessage(mailMessage, mailMessage.getAllRecipients());
+        }
+        catch (Exception ex) {
+           ex.printStackTrace();
+        }
+
+   }
+
+}
+
+/*
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendEmail
+{
+    public static void sendEmail(String toEmail,String subject,String  message)
+    {
+        try
+        {
+            String fromEmail="yourmail@gmail.com";
+            String username="yourmail@gmail.com";
+            String password="yourpassword";
+            Properties props= System.getProperties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.fallback", "false");
+            props.put( "mail.smtp.ssl.enable", "true");
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(true);
+            Message mailMessage=new MimeMessage(mailSession);
+            mailMessage.setFrom(new InternetAddress(fromEmail));
+            mailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            mailMessage.setContent(message, "text/plain");
+            mailMessage.setSubject(subject);
+            Transport transport=mailSession.getTransport("smtp");
+            transport.connect("smtp.gmail.com",username,password);
+            transport.sendMessage(mailMessage, mailMessage.getAllRecipients());
+        }
+        catch (Exception ex) {
+           ex.printStackTrace();
+        }
+   }
+}
+
+
+
+
+
+ */
